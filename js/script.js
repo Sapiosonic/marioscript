edccbf5864f0d37ac4f7c8fff961fc82618f8442
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-08-27 22:59:45 -0300

    adding events to Mario

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const mario = document.querySelector('.super-mario');
-const pipe = document.querySelector('.pipe-game');
+const mario = document.querySelector(".super-mario");
+const pipe = document.querySelector(".pipe-game");
 
 //criando uma arrow function para controlar o pulo do Mário
 const jump = () => {
@@ -10,4 +10,28 @@ const jump = () => {
   }, 500);
 }
 
+//para a execução no momento da execução
+//com o setInterval criamos um intervalo entre a execução
+const loopGame = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+  if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+  pipe.style.animation = "none";
+  pipe.style.left = `${pipePosition}px`;
+
+  mario.style.animation = "none";
+  mario.styel.bottom = `${marioPosition}px`;
+
+  mario.src = "./img/mario-game-over.png";
+  mario.style.width = "75px";
+  mario.style.margiLeft = "45px";
+
+  clearInterval(loopGame);
+
+  }
+
+}, 10);
+
 document.addEventListener("keydown", jump);
+
